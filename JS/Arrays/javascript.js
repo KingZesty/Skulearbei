885bcbf827f0d6f92d38a2a0f67274aa825de339
Ole Martin Kringlethaugen <Ole.Martin11@hotmail.com>
2017-02-14 13:23:49 +0100

    Starta arrays

---
+++ JS/Arrays/javascript.js
@@ -0,0 +1,10 @@
+var arr = [
+  ("Skoda", " Volvo", "Bmw"),
+  ("Octavia", " 240", " 3-Serie"),
+  ("2002 ", " 1997", " 2017")
+];
+
+
+function skriv() {
+ document.getElementById("huck").innerHTML = arr;
+}
