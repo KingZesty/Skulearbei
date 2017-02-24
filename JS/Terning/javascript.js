ace3420814e7e16ff7855190b5a4a267f5caa096
Ole Martin Kringlethaugen <Ole.Martin11@hotmail.com>
2017-02-24 12:51:00 +0100

    Terning changes

--- JS/Terning/javascript.js
+++ JS/Terning/javascript.js
@@ -1,7 +1,19 @@
+var terningkast = [0, 0, 0, 0, 0, 0];
+//lager en array
+
 function random() {
+  //opprette et random tall mellom 1 og 6
   terning = Math.floor((Math.random() * 6) + 1);
   console.log(terning);
+  //viser det randome tallet i console
+  terningkast[terning-1]++;
 
+  document.getElementById("terning").innerHTML = "";
+
+  for (var i=0; i<terningkast.length; i++){
+    document.getElementById("terning").innerHTML += "Antall " + (i+1) + ": " + terningkast[i] + "<br>";
+  }
+//viser terning bilder på de forskjellige tallene
   if (terning == 1) {
     document.getElementById("resultat").innerHTML= "Du Kasta " + terning + "<br> <img src='https://olspalten.files.wordpress.com/2012/11/1nye.png' alt='terning1' height='80' width='130'>";
   }
