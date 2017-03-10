88240867636ee147d99f59a30a33f1efe043fd6e
Ole Martin Kringlethaugen <Ole.Martin11@hotmail.com>
2017-03-10 08:49:11 +0100

    Lagt te stats

--- JS/Terning/javascript.js
+++ JS/Terning/javascript.js
@@ -32,4 +32,60 @@ function random() {
   else if (terning == 6) {
     document.getElementById("resultat").innerHTML= "Du Kasta " + terning + "<br> <img src='http://blog.winesworld.no/wp-content/uploads/2016/01/terning-6.png' alt='terning6' height='80' width='130'>";
   }
+
+  Highcharts.chart('statistikk', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Antall Terningkast'
+    },
+    subtitle: {
+        text: 'Antall Kast per Terningverdi'
+    },
+    xAxis: {
+        categories: ['1', '2', '3', '4', '5', '6'],
+        title: {
+            text: null
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Antall Ganger',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' kast'
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Kast',
+        data: [terningkast[0], terningkast[1], terningkast[2], terningkast[3], terningkast[4], terningkast[5]]
+    }]
+});
 }
